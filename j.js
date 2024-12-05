9a26d3544c8cb2c21eecb3049fe362f7925ec379
Jercel2A <jercellatoza473@gmail.com>
2024-12-05 08:09:34 +0800

    Add files via upload

---
+++ j.js
@@ -0,0 +1,11 @@
+unction append(value){
+    display.value += value
+}
+function calculate(){
+    display.value = eval(display.value)
+    
+}
+function clearDisplay() {
+      currentInput = '';
+      display.value = '';
+}
